9a18a90334588e38c3b1a186ff0ea4c92be28819
Jaypatel123 <jaypatel97043@gmail.com>
2023-12-19 19:12:53 -0500

    adding RESTAPI CALLS using Docker container

--- Django-admin/admin/products/views.py
+++ Django-admin/admin/products/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
     
     def create(self, request):
-        pass
-    
-    def retrive(self, request, pk=None):
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+    def retrieve(self, request, pk=None):
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
     
     def update(self, request, pk=None):
         pass
     
-    def delete(self, request, pk=None):
+    def destroy(self, request, pk=None):
         pass
