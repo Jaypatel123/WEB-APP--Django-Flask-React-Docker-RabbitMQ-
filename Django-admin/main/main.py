2d44676771204a118719b6144ac3f4adb54a53b2
Jaypatel123 <jaypatel97043@gmail.com>
2023-12-20 14:51:08 -0500

    docker flask-main working code

--- Django-admin/main/main.py
+++ Django-admin/main/main.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
+CORS(app)
+
+db = SQLAlchemy(app)
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
 @app.route('/')
-
 def index():
     return 'Hello'
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
