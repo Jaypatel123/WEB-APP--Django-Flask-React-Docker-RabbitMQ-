14557056d9ddec461c1cb679e37bb6072d112fe5
Jaypatel123 <jaypatel97043@gmail.com>
2023-12-19 20:25:40 -0500

    Added all the API

--- Django-admin/admin/products/views.py
+++ Django-admin/admin/products/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
-
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
         products = Product.objects.all()
@@ -18,12 +17,18 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
     def retrieve(self, request, pk=None):
-        product = Product.objects.get(pk=pk)
+        product = Product.objects.get(id=pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
     
     def update(self, request, pk=None):
-        pass
-    
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        
     def destroy(self, request, pk=None):
-        pass
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
